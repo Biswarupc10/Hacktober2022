5146d48d721847812f1201a782ed8703e4462639
Biswarup Chakraborty <biswarupc10@gmail.com>
2022-10-25 12:11:27 +0530

    1st commit

---
+++ cALCULATOR.java
@@ -0,0 +1,21 @@
+package practice;
+import java.util.*;
+public class cALCULATOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter your choice");
+      System.out.println("1st number");
+      int a=sc.nextInt();
+      System.out.println("2nd number");
+      int b=sc.nextInt();
+      char ch=' ';
+      switch(ch) {
+      case 1:if(ch=='+') {
+    	  
+      }
+	}
+
+}
+}
